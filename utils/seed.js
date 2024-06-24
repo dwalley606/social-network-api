cc5ecf132cdceb6612f43b80240fa5581f2939b2
Daniel <dwalley606@gmail.com>
2024-06-24 18:21:09 -0400

    Started the seed and data files

---
+++ utils/seed.js
@@ -0,0 +1,22 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("Connected");
+
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("users");
+  }
+
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection("thoughts");
+  }
+});
