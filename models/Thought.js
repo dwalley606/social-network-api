95c5ce49f4aade2276ccc73be318c87dd1666d79
Daniel <dwalley606@gmail.com>
2024-06-23 22:15:33 -0400

    Set up thought and user models, added connection, routes, and utils files

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, Types } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
